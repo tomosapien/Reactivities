7de5a218fc3cb38fda906120cf0d6ed2d47f847c
Tom Smit <tomosapien_66@hotmail.com>
2023-01-18 21:45:03 +0100

    Added axios and http request

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt => opt.AddPolicy("CorsPolicy", policy => {
+    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+}));
 
 var app = builder.Build();
 
@@ -21,6 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
